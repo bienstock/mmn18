8a799a26b6af097fd0cd14e6b7a266196a195dde
Gad Maor <gadm@il.ibm.com>
2016-07-27 01:29:03 +0300

    test scanner - added foretext

--- src/main/java/com/gadm/dsaalg/App.java
+++ src/main/java/com/gadm/dsaalg/App.java
@@ -9,6 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.print("Please enter a line of text: " );
 	Scanner sc = new Scanner(System.in);
 	String x = sc.nextLine();
         System.out.println( x );
