d51d57436eb5d4d6c429adf79dae3ad7a9ba4992
Gad Maor <gadm@il.ibm.com>
2016-07-27 01:13:28 +0300

    Java test project - 1st commit

---
+++ src/main/java/com/gadm/dsaalg/App.java
@@ -0,0 +1,13 @@
+package com.gadm.dsaalg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
